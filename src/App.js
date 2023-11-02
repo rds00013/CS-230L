9043c7f669a7af50b51c22ea7524338163cb1b97
rds00013 <rds00013@mix.wvu.edu>
2023-11-02 19:07:43 -0400

    Lav 6 Commit 3

--- src/App.js
+++ src/App.js
@@ -2,60 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
 
+import Navbar from "./Navbar";
+import Card from "./Card";
+
 function App() {
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#">Disabled</a>
-            </li>
-          </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </input>
-          </form>
-        </div>
-      </nav>
-
-      <h1>CS 230L</h1>
-      <h2>Section - 003</h2>
-      <p>WVU ID: 800365907</p>
-      <p>Hi I am Ross Shaner</p>
-
-      <div className="card" style="width: 18rem;">
-        <img className="card-img-top" src="..." alt="Card image cap">
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
-          </div>
-        </img>
-      </div>
+        <div className="Navbar"></div>
+          <h1>CS 230L</h1>
+          <h2>Section - 003</h2>
+          <p>WVU ID: 800365907</p>
+          <p>Hi I am Ross Shaner</p>
+        <div className="Card"></div>
     </div>
   );
 }
