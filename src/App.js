c64aa9a0a6f926276f6be6cc55ac198f2c4cd5f1
rds00013 <rds00013@mix.wvu.edu>
2023-11-02 19:16:49 -0400

    Lab 6 Commit 4

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import Card from "./Card";
 
 function App() {
   return (
-    <div className="App">
-        <div className="Navbar"></div>
+      <div>
+        <Navbar />
           <h1>CS 230L</h1>
           <h2>Section - 003</h2>
           <p>WVU ID: 800365907</p>
           <p>Hi I am Ross Shaner</p>
-        <div className="Card"></div>
+        <Card />
     </div>
   );
 }
