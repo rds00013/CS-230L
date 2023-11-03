9d66ab159a11ea8dbcb70c5f382f564dc953355f
rds00013 <142414965+rds00013@users.noreply.github.com>
2023-11-03 09:46:38 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.css";
-
 import Navbar from "./Navbar";
 import Card from "./Card";
 
 function App() {
   return (
-      <div className="App">
-        <Navbar />
-        <h1>CS 230L</h1>
-        <h2>Section - 003</h2>
-        <p>WVU ID: 800365907</p>
-        <p>Hi I am Ross Shaner</p>
-        <Card />
+    <div className="App">
+      <Navbar />
+      <h1>CS 230L</h1>
+      <h2>Section - 003</h2>
+      <p>WVU ID: 800365907</p>
+      <p>Hi I am Ross Shaner</p>
+      <Card />
     </div>
   );
 }
