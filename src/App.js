9f4d4ba9a89ce70e3c6c5edbae6f00eccabacce8
rds00013 <rds00013@mix.wvu.edu>
2023-11-03 22:06:54 -0400

    Lab 6 Submission 2

--- src/App.js
+++ src/App.js
@@ -4,15 +4,6 @@ import Card from "./Card";
 
 function App() {
   return (
-<<<<<<< HEAD
-      <div className="App">
-          <Navbar />
-        <h1>CS 230L</h1>
-        <h2>Section - 003</h2>
-        <p>WVU ID: 800365907</p>
-        <p>Hi I am Ross Shaner</p>
-          <Card />
-=======
     <div className="App">
       <Navbar />
       <h1>CS 230L</h1>
@@ -20,7 +11,6 @@ function App() {
       <p>WVU ID: 800365907</p>
       <p>Hi I am Ross Shaner</p>
       <Card />
->>>>>>> 4c8aa8760910ba3cc7ea2609f019ecaba95b4823
     </div>
   );
 }
