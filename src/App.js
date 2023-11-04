ff5dc34e73dc1bf00ee20e63522b440a2ed246ff
rds00013 <rds00013@mix.wvu.edu>
2023-11-03 22:00:02 -0400

    Lab 6 Submission

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import Card from "./Card";
 function App() {
   return (
       <div className="App">
-        <Navbar />
+          <Navbar />
         <h1>CS 230L</h1>
         <h2>Section - 003</h2>
         <p>WVU ID: 800365907</p>
         <p>Hi I am Ross Shaner</p>
-        <Card />
+          <Card />
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,20 +1,29 @@
 function Card() {
     return (
-        <div className="card text-white bg-primary mb-3" style="max-width: 18rem;">
-            <div className="card-header">Header</div>
-            <div className="card-body">
-                <h5 className="card-title">Primary card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div className="card-deck">
+            <div className="card">
+                <div className="card-header">
+                    <h5 className="card-title">Card 1</h5>
+                </div>
+                <div className="card-body">
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
             </div>
-            <div className="card-header">Header</div>
-            <div className="card-body">
-                <h5 className="card-title">Primary card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div className="card">
+                <div className="card-header">
+                    <h5 className="card-title">Card 2</h5>
+                </div>
+                <div className="card-body">
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
             </div>
-            <div className="card-header">Header</div>
-            <div className="card-body">
-                <h5 className="card-title">Primary card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div className="card">
+                <div className="card-header">
+                    <h5 className="card-title">Card 3</h5>
+                </div>
+                <div className="card-body">
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
             </div>
         </div>
     );
